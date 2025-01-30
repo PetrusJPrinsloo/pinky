24f3526b88d03e7ae328680b1a142d652657ddd9
Petrus Prinsloo <petrus.prinsloo@gmail.com>
2025-01-30 11:40:22 +0100

    Basic lexer done but WIP

---
+++ pinky.py
@@ -0,0 +1,17 @@
+import sys
+from tokens import *
+from lexer import *
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        raise SystemExit("Usage: pinky <filename>")
+    filename = sys.argv[1]
+    print(filename)
+
+    with open(filename) as file:
+        source = file.read()
+
+        print("LEXER:")
+        toks = Lexer(source).tokenize()
+        for tok in toks:
+            print(tok)

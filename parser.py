a64639b68f502eed14b9f81ecc49c4475fa7759d
Petrus Prinsloo <petrus.prinsloo@gmail.com>
2025-02-06 12:34:24 +0100

    Working on parser, still need helper functions filled in.

--- parser.py
+++ parser.py
@@ -1,4 +1,7 @@
-from tokens import Token
+from _ast import UnaryOp
+
+from tokens import Token, TOK_PLUS, TOK_SLASH, TOK_STAR, TOK_NOT, TOK_MINUS, TOK_INTEGER, TOK_FLOAT, TOK_LPAREN, \
+    TOK_RPAREN
 from model import *
 
 class Parser:
@@ -6,21 +9,52 @@ class Parser:
         self.tokens = tokens
         self.current = 0
 
+    # <primary>  ::=  <integer> | <float> | '(' <expr> ')'
     def primary(self):
-        pass
+        if self.match(TOK_INTEGER): return Integer(int(self.previous_token().lexeme))
+        if self.match(TOK_FLOAT): return Float(float(self.previous_token().lexeme))
+        if self.match(TOK_LPAREN):
+            expr = self.expr()
+            if not self.match(TOK_RPAREN):
+                raise SyntaxError(f'Error: ")" expected.')
+            else:
+                return Grouping(expr)
 
+    # <unary>  ::=  ('+'|'-'|'~') <unary>  |  <primary>
     def unary(self):
-        pass
+        if self.match(TOK_NOT) or self.match(TOK_MINUS) or self.match(TOK_MINUS):
+            op = self.previous_token()
+            operand = self.unary()
+            return UnaryOp(op, operand)
+        return self.primary()
 
     def factor(self):
-        pass
+        return self.unary()
 
+    # <term>  ::=  <factor> ( ('*'|'/') <factor> )*
     def term(self):
-        pass
+        expr = self.factor()
+        while self.match(TOK_STAR) or self.match(TOK_SLASH):
+            op = self.previous_token()
+            right = self.factor()
+            expr = BinOp(op, expr, right)
+        return expr
 
+    # <expr>  ::=  <term> ( ('+'|'-') <term> )*
     def expr(self):
-        pass
+        expr = self.term()
+        while self.match(TOK_PLUS) or self.match(TOK_SLASH):
+            op = self.previous_token()
+            right = self.term()
+            expr = BinOp(op, expr, right)
+        return expr
 
     def parse(self):
         ast = self.expr()
         return ast
+
+    def match(self, TOK_PLUS):
+        pass
+
+    def previous_token(self):
+        pass
